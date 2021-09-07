2159ebac8eaa2e166a67b9d456b32511f3c85959
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2021-09-06 17:51:49 -0700

    add custom SearchInput component and use in App

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput} from 'react-native';
+import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
   render() {
@@ -13,12 +14,8 @@ export default class App extends React.Component {
         </Text>
         <Text style={[styles.largeText, styles.textStyle]}>72°</Text>
 
-        <TextInput 
-          autoCorrect={false}
-          placeholder="Search any city"
-          placeholderTextColor="white"
-          style={styles.textInput}
-          clearButtonMode="always"
+        <SearchInput
+          placeholderLabelText="Enter a city name"
         />
       </KeyboardAvoidingView>
     );
@@ -42,14 +39,4 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18,
   },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
-  }
 });
