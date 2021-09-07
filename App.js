0b633fa780dca8580bb58f915aab22c03187c9e3
Nathan Krishnan <nathan.a.krishnan@gmail.com>
2021-09-06 23:04:02 -0700

    use ImageBackground and update styles in View

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput} from 'react-native';
+import { KeyboardAvoidingView, View, Platform, StyleSheet, Text, ImageBackground} from 'react-native';
+
 import SearchInput from './components/SearchInput';
+import getImageForWeather from './utils/getImageForWeather';
 
 export default class App extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="height">
-        <Text style={[styles.largeText, styles.textStyle]}>
-          San Francisco
-        </Text>
-        <Text style={[styles.smallText, styles.textStyle]}>
-          Sunny
-        </Text>
-        <Text style={[styles.largeText, styles.textStyle]}>72°</Text>
+        <ImageBackground 
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              San Francisco
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Sunny
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>72°</Text>
 
-        <SearchInput
-          placeholderLabelText="Enter a city name"
-        />
+            <SearchInput placeholderLabelText="Enter a city name"/>
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -25,18 +33,31 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E',
   },
   textStyle: {
     textAlign: 'center',
     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
   },
   smallText: {
     fontSize: 18,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
   },
 });
